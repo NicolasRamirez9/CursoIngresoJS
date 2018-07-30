a82d9cb8329e1cb8d09eb7c4870f80093753bbe2
NicolasRamirez9 <nicocai98@gmail.com>
2018-07-30 20:04:16 -0300

    ejercicios while.

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador = 0;
+
+
+	while (contador<11) 
+	{
+		alert("Numero: "+contador);
+		
+		contador = contador+1;
+	}
+
+
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador = 11;
+
+	while (contador<11 && contador>0)
+	{
+		alert("Numero: "+contador);
+
+		contador = contador-1;
+	}
+	
 
 
 }//FIN DE LA FUNCIÓN
