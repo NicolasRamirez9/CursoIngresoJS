39f96956f60f640c62cc4ae02488d3f2392cf55e
NicolasRamirez9 <nicocai98@gmail.com>
2018-08-02 11:06:09 -0300

    ejercicios.

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,60 @@
 function mostrar()
 {
 
-	var contador=0;
+	var contadorN=0;
+	var contadorP=0;
+	var contador0=0;
+	var contadorPosi=0;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
+	var negativos;
+	var positivos;
+	var promedioPosi;
+	var promedioNeg;
+	var dif;
 
 	while(respuesta!="no")
 	{
+		numero = prompt("Ingrese numero: ");
+
+		numero = parseInt(numero);
 		
+		respuesta = prompt("Desea seguir?");
+
+		if ( numero < 0){
+
+			negativos = negativos + numero;
+
+			contadorN ++;
+
+	} if ( numero > 0){
+
+		positivos = positivos + numero;
+
+		contadorP ++;
+
+	} if (contador==0){
+
+		contador0 ++;
+
+	} if (numero % 2 == 0 && numero != 0){
+		contadorPosi ++;
+
+	} promedioPosi = positivos / contadorP;
 	
+	promedioNeg = negativos / contadorN;
+	
+	dif = positivos - negativos;
+
 	}
+
+	Document.write ("Suma de negativos: " + negativos + "<br>" + "Suma de positivos: " + positivos + "<br>" + "Cantidad de positivos: " + contadorP + "<br>" + "Cantidad de negativos: " + contadorN + "<br>" + "Cantidad de 0: " + contador0 + "<br>" + contadorPosi + "<br>" + "Promedio de negativos: " + promedioNeg + "<br>" + "Promedio de positivos: " + promedioPosi + "<br>" + "Diferencia entre positivos y negativos: " + dif)
+
+
+
+
+
 
 
 
