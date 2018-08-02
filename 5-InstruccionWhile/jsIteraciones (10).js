60a6697e9c153a73658eff7fc7e41f59d39cff83
NicolasRamirez9 <nicocai98@gmail.com>
2018-08-02 11:36:13 -0300

    ejercicios.

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -4,12 +4,12 @@ function mostrar()
 	var contadorN=0;
 	var contadorP=0;
 	var contador0=0;
-	var contadorPosi=0;
+	var contadorPares=0;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
-	var negativos;
-	var positivos;
+	var negativos=0;
+	var positivos=0;
 	var promedioPosi;
 	var promedioNeg;
 	var dif;
@@ -19,7 +19,7 @@ function mostrar()
 		numero = prompt("Ingrese numero: ");
 
 		numero = parseInt(numero);
-		
+
 		respuesta = prompt("Desea seguir?");
 
 		if ( numero < 0){
@@ -28,18 +28,19 @@ function mostrar()
 
 			contadorN ++;
 
-	} if ( numero > 0){
+	} else if ( numero > 0){
 
 		positivos = positivos + numero;
 
 		contadorP ++;
 
-	} if (contador==0){
+	} else if (numero==0){
 
 		contador0 ++;
 
 	} if (numero % 2 == 0 && numero != 0){
-		contadorPosi ++;
+		
+		contadorPares ++;
 
 	} promedioPosi = positivos / contadorP;
 	
@@ -47,9 +48,13 @@ function mostrar()
 	
 	dif = positivos - negativos;
 
+	parseInt(promedioPosi);
+	parseInt(promedioNeg);
+	parseInt(dif);
+
 	}
 
-	Document.write ("Suma de negativos: " + negativos + "<br>" + "Suma de positivos: " + positivos + "<br>" + "Cantidad de positivos: " + contadorP + "<br>" + "Cantidad de negativos: " + contadorN + "<br>" + "Cantidad de 0: " + contador0 + "<br>" + contadorPosi + "<br>" + "Promedio de negativos: " + promedioNeg + "<br>" + "Promedio de positivos: " + promedioPosi + "<br>" + "Diferencia entre positivos y negativos: " + dif)
+	document.write ("Suma de negativos: " + negativos + "<br>" + "Suma de positivos: " + positivos + "<br>" + "Cantidad de positivos: " + contadorP + "<br>" + "Cantidad de negativos: " + contadorN + "<br>" + "Cantidad de 0: " + contador0 + "<br>" + "Cantidad de numeros pares: " + contadorPares + "<br>" + "Promedio de negativos: " + promedioNeg + "<br>" + "Promedio de positivos: " + promedioPosi + "<br>" + "Diferencia entre positivos y negativos: " + dif)
 
 
 
