c5aa4208a306ccae573f26fb425eedd354f9ddb9
NicoGabriel9 <nicocai98@gmail.com>
2018-08-06 03:41:39 -0300

    parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+    var preciofinal;
+
+    var precio = prompt("Ingresar precio: ");
+
+    var descuento = prompt("Ingresar descuento: ");
+
+    preciofinal = precio * (descuento / 100);
+
+    document.getElementById("elPrecioFinal").value = preciofinal
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+    var largo = prompt("Ingrese largo del triangulo: ");
+
+    var ancho = prompt("Ingrese ancho del triangulo: ");
+
+    var perimetro = (parseInt(largo) + parseInt(ancho)) * 2;
+
+
+    alert("El perimetro del rectangulo es de: " + perimetro);
+
 
 }
