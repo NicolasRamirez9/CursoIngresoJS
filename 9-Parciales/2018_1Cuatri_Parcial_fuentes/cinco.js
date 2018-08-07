ac00d2720a85b05e84eb9f4e32e149d2d6e72ca8
NicoGabriel9 <nicocai98@gmail.com>
2018-08-07 18:15:57 -0300

    parciales.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,38 @@
 function mostrar()
 {
+    var planeta = prompt("Ingrese planeta: ");
+
+    switch(planeta){
+
+        case "tierra":
+
+            alert("Aca vivimos.");
+            break;
+
+        case "mercurio":
+
+        case "venus":
+
+        alert("Aca hace mas calor.");
+        break;
+
+        case "marte":
+
+        case "jupiter":
+        
+        case "saturno":
+
+        case "urano":
+
+        case "neptuno":
+
+        alert("Aca hace mas frio.");
+        break;
+
+        default:
+
+        alert("Ingrese un planeta valido.");
+
+    }
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
+    var num1 = prompt("Ingrese numero: ");
 
-}
+    var num2 = prompt("Ingrese otro numero: ");
+
+    var suma;
+
+    var resta;
+
+
+    if (num1 == num2){
+        
+        alert(num1 + num2);
+
+    }if (num1 > num2){
+        
+        resta = (parseInt(num1) - parseInt(num2));
+
+        alert(resta);
+    
+    }else {
+        
+        suma = (parseInt(num1) + parseInt(num2));
+        
+        alert(suma);
+    
+    }if (suma > 10){  
+
+        alert("La suma es " + suma + " y supero el 10.");
+    }
+    }
